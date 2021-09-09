a91e82adabac30fd423158fa9f32e251ae044663
Chad <chadananda@gmail.com>
2021-09-09 11:02:34 -0700

    replaced all http with https

--- static/html/assets/plugins/gmaps/gmaps.js
+++ static/html/assets/plugins/gmaps/gmaps.js
@@ -12,14 +12,14 @@
 
 /*!
  * GMaps.js v0.4.9
- * http://hpneo.github.com/gmaps/
+ * https://hpneo.github.com/gmaps/
  *
  * Copyright 2013, Gustavo Leon
  * Released under the MIT License.
  */
 
 if (!(typeof window.google === 'object' && window.google.maps)) {
-  throw 'Google Maps API is required. Please register the following JavaScript library http://maps.google.com/maps/api/js?sensor=true.'
+  throw 'Google Maps API is required. Please register the following JavaScript library https://maps.google.com/maps/api/js?sensor=true.'
 }
 
 var extend_object = function(obj, new_obj) {
@@ -263,7 +263,7 @@ var GMaps = (function(global) {
       if (!getElementById('gmaps_context_menu')) return;
 
       var context_menu_element = getElementById('gmaps_context_menu');
-      
+
       context_menu_element.innerHTML = html;
 
       var context_menu_items = context_menu_element.getElementsByTagName('a'),
@@ -300,11 +300,11 @@ var GMaps = (function(global) {
 
         var overlay = new google.maps.OverlayView();
         overlay.setMap(self.map);
-        
+
         overlay.draw = function() {
           var projection = overlay.getProjection(),
               position = e.marker.getPosition();
-          
+
           e.pixel = projection.fromLatLngToContainerPixel(position);
 
           buildContextMenuHTML(control, e);
@@ -520,7 +520,7 @@ GMaps.prototype.addControl = function(options) {
 
   var control = this.createControl(options);
   this.controls.push(control);
-  
+
   this.map.controls[position].push(control);
 
   return control;
@@ -587,7 +587,7 @@ GMaps.prototype.createMarker = function(options) {
           if(!me.pixel){
             me.pixel = map.getProjection().fromLatLngToPoint(me.latLng)
           }
-          
+
           options[name].apply(this, [me]);
         });
       }
@@ -737,7 +737,7 @@ GMaps.prototype.drawOverlay = function(options) {
     if (!options.layer) {
       options.layer = 'overlayLayer';
     }
-    
+
     var panes = this.getPanes(),
         overlayLayer = panes[options.layer],
         stop_overlay_events = ['contextmenu', 'DOMMouseScroll', 'dblclick', 'mousedown'];
@@ -1333,7 +1333,7 @@ GMaps.prototype.drawRoute = function(options) {
           strokeOpacity: options.strokeOpacity,
           strokeWeight: options.strokeWeight
         });
-        
+
         if (options.callback) {
           options.callback(e[e.length - 1]);
         }
@@ -1388,7 +1388,7 @@ GMaps.prototype.travelRoute = function(options) {
 
 GMaps.prototype.drawSteppedRoute = function(options) {
   var self = this;
-  
+
   if (options.origin && options.destination) {
     this.getRoutes({
       origin: options.origin,
@@ -1533,7 +1533,7 @@ GMaps.prototype.toImage = function(options) {
 
   if (this.markers.length > 0) {
     static_map_options['markers'] = [];
-    
+
     for (var i = 0; i < this.markers.length; i++) {
       static_map_options['markers'].push({
         lat: this.markers[i].getPosition().lat(),
@@ -1544,7 +1544,7 @@ GMaps.prototype.toImage = function(options) {
 
   if (this.polylines.length > 0) {
     var polyline = this.polylines[0];
-    
+
     static_map_options['polyline'] = {};
     static_map_options['polyline']['path'] = google.maps.geometry.encoding.encodePath(polyline.getPath());
     static_map_options['polyline']['strokeColor'] = polyline.strokeColor
@@ -1558,7 +1558,7 @@ GMaps.prototype.toImage = function(options) {
 GMaps.staticMapURL = function(options){
   var parameters = [],
       data,
-      static_root = 'http://maps.googleapis.com/maps/api/staticmap';
+      static_root = 'https://maps.googleapis.com/maps/api/staticmap';
 
   if (options.url) {
     static_root = options.url;
@@ -1568,7 +1568,7 @@ GMaps.staticMapURL = function(options){
   static_root += '?';
 
   var markers = options.markers;
-  
+
   delete options.markers;
 
   if (!markers && options.marker) {
@@ -1944,7 +1944,7 @@ GMaps.geocode = function(options) {
   delete options.lat;
   delete options.lng;
   delete options.callback;
-  
+
   this.geocoder.geocode(options, function(results, status) {
     callback(results, status);
   });
@@ -1954,7 +1954,7 @@ GMaps.geocode = function(options) {
 // Polygon containsLatLng
 // https://github.com/tparkin/Google-Maps-Point-in-Polygon
 // Poygon getBounds extension - google-maps-extensions
-// http://code.google.com/p/google-maps-extensions/source/browse/google.maps.Polygon.getBounds.js
+// https://code.google.com/p/google-maps-extensions/source/browse/google.maps.Polygon.getBounds.js
 if (!google.maps.Polygon.prototype.getBounds) {
   google.maps.Polygon.prototype.getBounds = function(latLng) {
     var bounds = new google.maps.LatLngBounds();
@@ -2060,6 +2060,6 @@ if (!Array.prototype.indexOf) {
       return -1;
   }
 }
-  
+
 return GMaps;
 }));
--- static/html/assets/plugins/jflickrfeed/jflickrfeed.js
+++ static/html/assets/plugins/jflickrfeed/jflickrfeed.js
@@ -1,7 +1,7 @@
 /*
 * Copyright (C) 2009 Joel Sutherland
 * Licenced under the MIT license
-* http://www.newmediacampaigns.com/page/jquery-flickr-plugin
+* https://www.newmediacampaigns.com/page/jquery-flickr-plugin
 *
 * Available tags for templates:
 * title, link, date_taken, description, published, author, author_id, tags, image*
@@ -9,7 +9,7 @@
 (function($) {
 	$.fn.jflickrfeed = function(settings, callback) {
 		settings = $.extend(true, {
-			flickrbase: 'http://api.flickr.com/services/feeds/',
+			flickrbase: 'https://api.flickr.com/services/feeds/',
 			feedapi: 'photos_public.gne',
 			limit: 20,
 			qstrings: {
@@ -39,7 +39,7 @@
 			$.getJSON(url, function(data){
 				$.each(data.items, function(i,item){
 					if(i < settings.limit){
-					
+
 						// Clean out the Flickr Description
 						if(settings.cleanDescription){
 							var regex = /<p>(.*?)<\/p>/g;
@@ -50,16 +50,16 @@
 									item.description = item.description.replace('<p>','').replace('</p>','');
 							}
 						}
-						
+
 						// Add Image Sizes
-						// http://www.flickr.com/services/api/misc.urls.html
+						// https://www.flickr.com/services/api/misc.urls.html
 						item['image_s'] = item.media.m.replace('_m', '_s');
 						item['image_t'] = item.media.m.replace('_m', '_t');
 						item['image_m'] = item.media.m.replace('_m', '_m');
 						item['image'] = item.media.m.replace('_m', '');
 						item['image_b'] = item.media.m.replace('_m', '_b');
 						delete item.media;
-						
+
 						// Use Template
 						if(settings.useTemplate){
 							var template = settings.itemTemplate;
@@ -69,7 +69,7 @@
 							}
 							$container.append(template)
 						}
-						
+
 						//itemCallback
 						settings.itemCallback.call(container, item);
 					}
